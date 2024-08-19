a724864b4a225e43fc8e4233358d5307206deb40
SIMMY XAVIER <109429735+siim2mary@users.noreply.github.com>
2024-08-19 10:33:25 +0530

    Update setup.sh added setup wheel

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Upgrade pip
-pip install --upgrade pip
+#pip install --upgrade pip
+
+# Upgrade pip, setuptools, and wheel
+pip install --upgrade pip setuptools wheel
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
